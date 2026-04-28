3327e3d800f4705ff4ac7ffdcf59938899e49f13
roaa-ka123 <roaaka633@gmail.com>
2026-04-28 22:17:27 +0200

    Delete auth_check.php

--- auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
-    header('Location: /Health_center_management_system/login.php');
-    exit;
-}
-?>
